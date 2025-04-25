a5eb18a38f4e38d8c3ee8d06426cfcb8100eeec7
Guillermo <ghgenet7@gmail.com>
2025-04-25 16:32:14 -0300

    Vuelvo

--- app.py
+++ app.py
@@ -522,4 +522,4 @@ def eliminar_usuario(id):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host='0.0..0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
