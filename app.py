6a3832b7c1ff7be46d0bb50cc3084fc7207c1e04
Guillermo Gutierrez <argutierrezg@tetrapak.com>
2025-08-11 21:55:29 -0300

    Agrego datos al contrato

--- app.py
+++ app.py
@@ -399,7 +399,7 @@ def dashboard():
         tarea = request.form.get('tarea', '').strip()
         cliente = request.form.get('cliente', '').strip()  # Si cliente es texto, OK
         comentarios = request.form.get('comentarios', '').strip()
-        contrato = bool(int(request.form.get("contrato")))
+        contrato = request.form.get("contrato", "").strip()
         service_order = request.form.get('service_order', '').strip()
 
         try:
