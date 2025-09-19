e3171a9f93672eb3fb90c3701560e1549d1a9289
Guillermo Gutiérrez <101758263+GUILLERMOWEB@users.noreply.github.com>
2025-09-19 12:54:25 +0000

    Arreglo exportar exel

--- app.py
+++ app.py
@@ -643,23 +643,23 @@ def exportar_excel():
         'Línea': r.linea or ''
     } for r in registros if r.user is not None])
 
-if es_admin:
-    df['Contable'] = df['Contable'].map({
-        '73450003': 'Contrato',
-        '79010000': 'Administrativo                         - 79010000',
-        '79200030': 'Issue Resolution - Service             - 79200030',
-        '79200020': 'Issue Resolution - Capital CARTON      - 79200020',
-        '79200050': 'Issue Resolution - Capital PROCESSING  - 79200050',
-        '580000': 'Capital Equipment carton                 - 580000',
-        '70350000': 'Capital Equipment processing           - 70350000',
-        '73450000': 'Fuera de contrato                      - 73450000',
-        '560000': 'Upgrade projects                         - 560000',
-        '70158000': 'Mandatory kit                          - 70158000',
-        '480000': 'Rebilling                                - 480000',
-        '73453000': 'Training Facturable                    - 73453000',
-        '70400010': 'MDR                                    - 70400010',
-        '0': 'N/A'
-    }).fillna(df['Contable'])
+    if es_admin:
+        df['Contable'] = df['Contable'].map({
+            '73450003': 'Contrato',
+            '79010000': 'Administrativo                         - 79010000',
+            '79200030': 'Issue Resolution - Service             - 79200030',
+            '79200020': 'Issue Resolution - Capital CARTON      - 79200020',
+            '79200050': 'Issue Resolution - Capital PROCESSING  - 79200050',
+            '580000': 'Capital Equipment carton                 - 580000',
+            '70350000': 'Capital Equipment processing           - 70350000',
+            '73450000': 'Fuera de contrato                      - 73450000',
+            '560000': 'Upgrade projects                         - 560000',
+            '70158000': 'Mandatory kit                          - 70158000',
+            '480000': 'Rebilling                                - 480000',
+            '73453000': 'Training Facturable                    - 73453000',
+            '70400010': 'MDR                                    - 70400010',
+            '0': 'N/A'
+        }).fillna(df['Contable'])
 
 
     archivo = BytesIO()
